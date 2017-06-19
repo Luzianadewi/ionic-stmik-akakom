3a5915aec721b69a30f76cdcfec2512cce1c3aa0
luzianadewi <luzianadewi9@gmail.com>
2017-06-19 21:02:23 +0700

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Sejarah } from '../pages/sejarah/sejarah';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
